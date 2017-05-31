68a8ff27f86d79c2b6c5fd7b8a7dc0b162d0ce55
lj <1259549061@qq.com>
2017-05-31 20:22:04 +0800

    引用less 更结构化css

--- src/components/index.vue
+++ src/components/index.vue
@@ -69,16 +69,11 @@
       </div>
     </nav>.
     <main>
-      <!--<span>京东秒杀</span>-->
-      <!--<span>16点场</span>-->
-
     </main>
     <footer>
-
     </footer>
   </div>
 </template>
-
 <script type="text/ecmascript-6">
 export default {
   data () {
@@ -87,140 +82,9 @@ export default {
       mess: '消息'
     }
   }
-
 }
 </script>
-<style>
-  header{
-    width: 100%;
-  }
-  .header_box{
-    width: 100%;
-    position: fixed;
-    z-index: 1000;
-  }
-  .sas_box{
-    width: 50px;
-    height: 50px;
-    position: absolute;
-    left: 0;
-    top: 0;
-  }
-  .xx_box{
-    width: 50px;
-    height: 50px;
-    position: absolute;
-    right: 0;
-    top: 0;
-  }
-  .sas_box > a ,.xx_box > a {
-    width: 30px;
-    height: 30px;
-    display: block;
-    position: relative;
-    left: 10px;
-    top: 5px;
-  }
-  .sas_box > a > img,.xx_box > a > img{
-    width: 30px;
-    height: 30px;
-  }
-  .input_box{
-    width: 100%;
-    padding-left: 50px;
-    padding-right: 50px;
-    box-sizing: border-box;
-  }
-
-  .input_box > input{
-    width: 100%;
-    height: 50px;
-    border-radius: 25px;
-    border:1px solid red;
-    -webkit-tap-highlight-color: transparent;
-    outline-color: transparent;
-  }
-  header .header_box div span{
-    font-size: 12px;
-    text-align: center;
-    display: block;
-    width: 100%;
-    position: relative;
-    top: 5px;;
-    color: #ffffff;
-  }
-  .lb_box{
-    width: 100%;
-    overflow: hidden;
-    z-index: 500;
-    position: relative;
-  }
-  .lb_box > ul {
-    width: 1000%;
-  }
-  .lb_box > ul > li{
-    width: 10%;
-    float: left;
-  }
-  .lb_box > ul > li > a{
-    width: 100%;
-    display: block;
-  }
-  .lb_box > ul > li > a > img{
-    width: 100%;
-    vertical-align: middle;
-  }
-  .lb_box > ul:nth-child(2){
-    width: 120px;
-    height: 10px;
-    position: absolute;
-    left: 50%;
-    bottom: 10px;
-    margin-left: -60px;
-  }
-  .lb_box > ul:nth-child(2) li{
-    width: 6px;
-    height: 6px;
-    border-radius: 3px;
-    border: 1px solid black;
-    margin-left: 32px;
-    box-sizing: border-box;
-  }
-  .lb_box > ul:nth-child(2) li:nth-child(1){
-    background-color: #ffffff;
-  }
-  .lb_box > ul:nth-child(2) li:first-child{
-    margin-left: 0px;
-  }
-  nav{
-    width: 100%;
-    background-color: bisque;
-  }
-  .dh_box{
-    width: 100%;
-  }
-  .dh_box  ul {
-    width: 100%;
-    display: flex;
-  }
-  .dh_box  ul li{
-    flex:1;
-    text-align: center;
-    margin-top: 10px;
-  }
-  .dh_box  ul:last-child{
-    padding-bottom: 10px;
-  }
-  .dh_box ul li a{
-    width: 100%;
-    display: block;
-  }
-  .dh_box ul li a img {
-    width: 40px;
-    height: 40px;
-  }
-  .dh_box ul li  > p{
-    margin-top: 4px;
-  }
+<style lang="less" rel="stylesheet/less" type="text/less">
+  @import "../less/app.less";
 </style>
 
--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import './css/app.css'
 
 /* eslint-disable no-new */
 new Vue({
